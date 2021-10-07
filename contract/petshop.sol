fee5fcec634a500707943b35bd48de2465e54df4
thompson <thompsonmina7@gmail.com>
2021-10-07 04:46:07 +0100

    added the safemath library and optimise the contract by variable packing the struct

--- contract/petshop.sol
+++ contract/petshop.sol
@@ -2,6 +2,64 @@
 
 pragma solidity >=0.7.0 <0.9.0;
 
+library SafeMath {
+  /**
+  * @dev Multiplies two numbers, reverts on overflow.
+  */
+  function mul(uint256 a, uint256 b) internal pure returns (uint256) {
+    // Gas optimization: this is cheaper than requiring 'a' not being zero, but the
+    // benefit is lost if 'b' is also tested.
+    // See: https://github.com/OpenZeppelin/openzeppelin-solidity/pull/522
+    if (a == 0) {
+      return 0;
+    }
+
+    uint256 c = a * b;
+    require(c / a == b);
+    return c;
+
+  }
+
+  /**
+  * @dev Integer division of two numbers truncating the quotient, reverts on division by zero.
+  */
+  function div(uint256 a, uint256 b) internal pure returns (uint256) {
+    require(b > 0); // Solidity only automatically asserts when dividing by 0
+    uint256 c = a / b;
+    // assert(a == b * c + a % b); // There is no case in which this doesn't hold
+    return c;
+
+  }
+
+  /**
+  * @dev Subtracts two numbers, reverts on overflow (i.e. if subtrahend is greater than minuend).
+  */
+  function sub(uint256 a, uint256 b) internal pure returns (uint256) {
+    require(b <= a);
+    uint256 c = a - b;
+    return c;
+  }
+
+  /**
+  * @dev Adds two numbers, reverts on overflow.
+  */
+  function add(uint256 a, uint256 b) internal pure returns (uint256) {
+    uint256 c = a + b;
+    require(c >= a);
+    return c;
+  }
+
+  /**
+  * @dev Divides two numbers and returns the remainder (unsigned integer modulo),
+  * reverts when dividing by zero.
+  */
+  function mod(uint256 a, uint256 b) internal pure returns (uint256) {
+    require(b != 0);
+    return a % b;
+  }
+
+}
+
 interface IERC20Token {
     function transfer(address, uint256) external returns (bool);
 
@@ -33,20 +91,23 @@ contract petShop {
     address internal petShopOwner;
     address internal cUsdTokenAddress = 0x874069Fa1Eb16D44d622F2e0Ca25eeA172369bC1;
 
+    using SafeMath for uint256;
+    
     constructor(uint256 _adoptionFEE) {
         petShopOwner = payable(msg.sender);
-        adoptionFEE = _adoptionFEE * (10**18);
+        adoptionFEE = _adoptionFEE.mul(10**18);
     }
 
     struct pet {
+        address adopter;
         uint id;
+        uint age;
         string petName;
         string imageURL;
-        uint age;
         string breed;
         string location;
         bool adopted;
-        address adopter;
+        
     }
 
     modifier onlyOwner() {
@@ -113,7 +174,7 @@ contract petShop {
     function getAdoptionFee() public view returns(uint256){
         return adoptionFEE;
     }
-
+    
     function petShopBalance() public view returns(uint256){
         return IERC20Token(cUsdTokenAddress).balanceOf(address(this));
     }
